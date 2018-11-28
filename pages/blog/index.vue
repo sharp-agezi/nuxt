55ed3b5c13545c3e46a90b5dcb1511ec66fff454
sharp-agezi <1150211686@qq.com>
2018-11-28 20:08:59 +0800

    blog

---
+++ pages/blog/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="container">
+    <div>
+      <logo/>
+      <h1 class="title">user</h1>
+      <h2 class="subtitle">My fine user project</h2>
+      <div class="links">
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  layout: 'blog'
+}
+</script>
+
